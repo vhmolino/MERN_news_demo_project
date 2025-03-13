a5aa49d9ded06707c51aecd94bc803ca723565e3
vhmolino <vichercar2003@gmail.com>
2025-03-13 02:24:02 +0100

    add news API integration and define news type

---
+++ frontend/src/api/news/get-news.ts
@@ -0,0 +1,18 @@
+import axios from "axios";
+import { New } from "../../types/news";
+
+export type ApiResponse<T> =
+  | { error: string; data: never }
+  | { data: T; error: never };
+
+const API_URL = `${import.meta.env.VITE_SERVER_URL}/news`;
+export const getNews = async (): Promise<ApiResponse<New[]>> => {
+  try {
+    const response = await axios.get<{ news: New[] }>(API_URL);
+    console.log(response.data.news);
+    return { data: response.data.news, error: undefined as never };
+  } catch (error) {
+    console.error("Error al obtener las noticias:", error);
+    return { error: "Error al obtener las noticias", data: undefined as never };
+  }
+};
