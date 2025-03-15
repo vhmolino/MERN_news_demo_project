4c3f8b641be53fbddd81e865f15d6b0f735dd525
vhmolino <vichercar2003@gmail.com>
2025-03-15 22:39:06 +0100

    make work the nabvar buttons

--- frontend/src/components/nabvar.tsx
+++ frontend/src/components/nabvar.tsx
@@ -1,6 +1,8 @@
 import { AppBar, Toolbar, Typography, Button, Box } from "@mui/material";
+import { useNavigate } from "react-router-dom";
 
 const Navbar = () => {
+  const navigate = useNavigate();
   return (
     <AppBar position="static" color="primary">
       <Toolbar>
@@ -8,9 +10,30 @@ const Navbar = () => {
           My News App
         </Typography>
         <Box>
-          <Button color="inherit">Inicio</Button>
-          <Button color="inherit">Noticias</Button>
-          <Button color="inherit">Archivadas</Button>
+          <Button
+            onClick={() => {
+              navigate("/");
+            }}
+            color="inherit"
+          >
+            Inicio
+          </Button>
+          <Button
+            onClick={() => {
+              navigate("/news");
+            }}
+            color="inherit"
+          >
+            Noticias
+          </Button>
+          <Button
+            onClick={() => {
+              navigate("/news/archived");
+            }}
+            color="inherit"
+          >
+            Archivadas
+          </Button>
         </Box>
       </Toolbar>
     </AppBar>
