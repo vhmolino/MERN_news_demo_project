83ad73e68c6d1661faa469e2ccb9ed6a8f3df146
vhmolino <vichercar2003@gmail.com>
2025-03-16 11:23:27 +0100

    add route for news internal component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./pages/home";
 import NewsListComponent from "./pages/news_component";
 import ArchivedNewsComponent from "./pages/archived_news_component";
+import NewsInternatComponent from "./pages/new_internal_component";
 
 const App = () => {
   return (
@@ -9,6 +10,7 @@ const App = () => {
       <Route path="/" element={<Home />} />
       <Route path="/news" element={<NewsListComponent />} />
       <Route path="/news/archived" element={<ArchivedNewsComponent />} />
+      <Route path="/news-detail" element={<NewsInternatComponent />} />
     </Routes>
   );
 };
