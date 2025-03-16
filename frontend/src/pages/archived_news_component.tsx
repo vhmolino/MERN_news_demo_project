27b250ef4823ffdf76c136c345096b220940a511
vhmolino <vichercar2003@gmail.com>
2025-03-16 19:43:30 +0100

    remove unused import statement from archived news component

--- frontend/src/pages/archived_news_component.tsx
+++ frontend/src/pages/archived_news_component.tsx
@@ -1,5 +1,3 @@
-// import { useState } from "react";
-
 import React from "react";
 import { enqueueSnackbar } from "notistack";
 import { Box, Grid, Pagination, Stack, Typography } from "@mui/material";
